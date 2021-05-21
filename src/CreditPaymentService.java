d9e5242b7f21a8bb5be54a7b5349323461ed1bcc
Alexandra Matyukhina <alexandra.paramygina@gmail.com>
2021-05-21 08:53:27 +0300

    Modified src/

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,7 +2,7 @@ public class CreditPaymentService {
     public double calculate(int creditSum, int monthAmount, int termDate) {
         double percentYear = 9.99;
         double percentMonth = ((percentYear * termDate) / monthAmount / 100);
-        double monthPayment = (int) (creditSum * (percentMonth + percentMonth / (Math.pow(1 + percentMonth, monthAmount) - 1)));
+        int monthPayment = (int) (creditSum * (percentMonth + percentMonth / (Math.pow(1 + percentMonth, monthAmount) - 1)));
         return monthPayment;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,13 @@ public class Main {
         double percentYear = 9.99;
         int termDate;
         int monthAmount;
-        double CreditPayment = service.calculate(1_000_000, 12, 1);
+        int CreditPayment = (int) service.calculate(1_000_000, 12, 1);
         System.out.println("Ежемесячный платеж по кредиту на сумму 1000000 сроком на один год составляет: " + CreditPayment);
 
-        double CreditPayment2 = service.calculate(1_000_000, 24, 2);
+        int CreditPayment2 = (int) service.calculate(1_000_000, 24, 2);
         System.out.println("Ежемесячный платеж по кредиту на сумму 1000000 сроком на два года составляет: " + CreditPayment2);
 
-        double CreditPayment3 = service.calculate(1_000_000, 36, 3);
+        int CreditPayment3 = (int) service.calculate(1_000_000, 36, 3);
         System.out.println("Ежемесячный платеж по кредиту на сумму 1000000 сроком на три года составляет: " + CreditPayment3);
     }
 }
